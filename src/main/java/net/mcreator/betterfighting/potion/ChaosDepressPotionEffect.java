9b67b5c8e74b23a5832ef961eb05e8d9adc5c4f9
c-chayet <98602683+c-chayet@users.noreply.github.com>
2022-05-22 11:16:47 +0800

    Add files via upload

---
+++ src/main/java/net/mcreator/betterfighting/potion/ChaosDepressPotionEffect.java
@@ -0,0 +1,64 @@
+
+package net.mcreator.fight_extend_destroyed_civilization.potion;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effect;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ChaosDepressPotionEffect {
+	@ObjectHolder("fight_extend_dc:chaos_depress")
+	public static final Effect potion = null;
+
+	@SubscribeEvent
+	public static void registerEffect(RegistryEvent.Register<Effect> event) {
+		event.getRegistry().register(new EffectCustom());
+	}
+
+	public static class EffectCustom extends Effect {
+		public EffectCustom() {
+			super(EffectType.BENEFICIAL, -3407617);
+			setRegistryName("chaos_depress");
+		}
+
+		@Override
+		public String getName() {
+			return "effect.chaos_depress";
+		}
+
+		@Override
+		public boolean isBeneficial() {
+			return true;
+		}
+
+		@Override
+		public boolean isInstant() {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRenderInvText(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRender(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRenderHUD(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean isReady(int duration, int amplifier) {
+			return true;
+		}
+	}
+}
